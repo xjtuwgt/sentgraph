b2b5676d2fd33d7c934c7ab50581761e4c98cb13
guangtao.wang <guangtao.wang@jd.com>
2021-05-26 16:32:21 -0700

    first commit

--- sent_graph_mhqa/offline_feature_extractor.py
+++ sent_graph_mhqa/offline_feature_extractor.py
@@ -129,7 +129,6 @@ def feature_extraction(args):
             sent_names = example_i.sent_names
             supp_sent_labels = supp_sent_np[idx].tolist()
             sent_num = sent_num_list[idx]
-            print(sent_num)
             graph_i = {'id': key, 'feat': sent_embed, 'number': sent_num, 'edges': edges, 'mask': sent_mask, 'names': sent_names, 'label': supp_sent_labels}
             graph_features.append(graph_i)
     return graph_features
